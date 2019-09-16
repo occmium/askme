a250b7f559393ac65a244643816c0caaecf5ad5a
occmium <occkart@yahoo.com>
2019-09-16 15:35:30 +0600

    Lesson_51 Added Create&Update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 class UsersController < ApplicationController
+
+  # Загружаем юзера из базы для экшенов кроме :index, :create, :new
+
+  before_action :load_user, except: [:index, :create, :new]
+
+  # Проверяем имеет ли юзер доступ к экшену, делаем это для всех действий, кроме
+  # :index, :new, :create, :show — к этим действиям есть доступ у всех, даже у
+  # тех, у кого вообще нет аккаунта на нашем сайте.
+  #
+  # before_action :authorize_user, except: [:index, :new, :create, :show]
+
   # def index
   #   # Создаём массив из двух болванок пользователей. Вызываем метод # User.new, который создает модель, не записывая её в базу.
   #   # У каждого юзера мы прописали id, чтобы сымитировать реальную
@@ -62,6 +73,7 @@ class UsersController < ApplicationController
   end
 
   def edit
+    # @user = User.find params[:id]
   end
 
   # def show
@@ -82,7 +94,46 @@ class UsersController < ApplicationController
     # @new_question = Question.new
   # end
 
+  def update
+    # @user = User.find params[:id]
+
+    # Аналогично create, мы получаем параметры нового (обновленного)
+    # пользователя с помощью метода user_params, и пытаемся обновить @user с
+    # этими значениями.
+    if @user.update(user_params)
+      # Если получилось, отправялем пользователя на его страницу с сообщением,
+      # что пользователь успешно обновлен.
+      redirect_to user_path(@user), notice: 'Данные обновлены'
+    else
+      # Если не получилось, как и в create рисуем страницу редактирования
+      # пользователя, на которой нам будет доступен объект @user, содержащий
+      # информацию об ошибках валидации, которые отобразит форма.
+      render 'edit'
+    end
+  end
+
+  def show
+    # @user = User.find params[:id]
+
+
+    # Достаем вопросы пользователя с помощью метода questions, который мы
+    # объявили в модели User (has_many :questions), у результата возврата этого
+    # метода вызываем метод order, который отсортирует вопросы по дате.
+    @questions = @user.questions.order(created_at: :desc)
+
+    # Для формы нового вопроса, которая есть у нас на странице пользователя,
+    # создаем болванку вопроса, вызывая метод build у результата вызова метода
+    # @user.questions.
+    @new_question = @user.questions.build
+  end
+
   private
+
+  # Загружаем из базы запрошенного юзера, находя его по params[:id].
+  def load_user
+    @user ||= User.find params[:id]
+  end
+
 
   # Явно задаем список разрешенных параметров для модели User. Мы говорим, что
   # у хэша params должен быть ключ :user. Значением этого ключа может быть хэш с
