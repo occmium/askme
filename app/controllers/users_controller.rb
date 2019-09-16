3041516df7a6f3aa1961ec0de4a4ff235f38f2a8
occmium <occkart@yahoo.com>
2019-09-16 14:38:16 +0600

    Lesson_51 Added Flash in application

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,45 +1,100 @@
 class UsersController < ApplicationController
-  def index
-    # Создаём массив из двух болванок пользователей. Вызываем метод # User.new, который создает модель, не записывая её в базу.
-    # У каждого юзера мы прописали id, чтобы сымитировать реальную
-    # ситуацию – иначе не будет работать хелпер путей
-    @users = [
-      User.new(
-        id: 1,
-        name: 'Anon',
-        username: 'deleted',
-        avatar_url: 'https://vk.com/images/deactivated_100.png?ava=1'
-      ),
+  # def index
+  #   # Создаём массив из двух болванок пользователей. Вызываем метод # User.new, который создает модель, не записывая её в базу.
+  #   # У каждого юзера мы прописали id, чтобы сымитировать реальную
+  #   # ситуацию – иначе не будет работать хелпер путей
+  #   @users = [
+  #     User.new(
+  #       id: 1,
+  #       name: 'Anon',
+  #       username: 'deleted',
+  #       avatar_url: 'https://vk.com/images/deactivated_100.png?ava=1'
+  #     ),
+  #
+  #     User.new(
+  #       id: 2,
+  #       name: 'Deanon',
+  #       username: 'undeleted'
+  #     )
+  #   ]
+  # end
 
-      User.new(
-        id: 2,
-        name: 'Deanon',
-        username: 'undeleted'
-      )
-    ]
+  def index
+    # запишем в неё всех пользователей
+    @users = User.all
   end
 
+  # Действие new будет отзываться по адресу /users/new
   def new
+    # Пока создадим новый экземпляр модели
+    @user = User.new
+  end
+
+  def create
+    # Если пользователь уже авторизован, ему не нужна новая учетная запись,
+    # отправляем его на главную с сообщением.
+
+
+
+
+    # redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
+
+
+
+
+
+    # Иначе, создаем нового пользователя с параметрами, которые нам предоставит
+    # метод user_params.
+    @user = User.new(user_params)
+
+    # Пытаемся сохранить пользователя.
+    if @user.save
+      # Если удалось, отправляем пользователя на главную с сообщение, что
+      # пользователь создан.
+      redirect_to root_url, notice: 'Пользователь успешно зарегистрирован!'
+    else
+      # Если не удалось по какой-то причине сохранить пользователя, то рисуем
+      # (обратите внимание, это не редирект), страницу new с формой
+      # пользователя, который у нас лежит в переменной @user. В этом объекте
+      # содержатся ошибки валидации, которые выведет шаблон формы.
+      render 'new'
+    end
   end
 
   def edit
   end
 
-  def show
-    @user = User.new(
-      name: 'Anon',
-      username: 'deleted',
-      avatar_url: 'https://vk.com/images/deactivated_100.png?ava=1'
+  # def show
+    # @user = User.new(
+    #   name: 'Anon',
+    #   username: 'deleted',
+    #   avatar_url: 'https://vk.com/images/deactivated_100.png?ava=1'
+    # )
+    #
+    # @questions = [
+    #   Question.new(text: 'Как дела?', created_at: Date.parse('13.09.2019')),
+    #   Question.new(text: 'В чём сила, брат?', created_at: Date.parse('14.09.2019')),
+    #   Question.new(text: 'Куришь?', created_at: Date.parse('15.09.2019')),
+    #   Question.new(text: 'Спишь?', created_at: Date.parse('15.09.2019')),
+    #   Question.new(text: 'Ешь?', created_at: Date.parse('15.09.2019'))
+    # ]
+    #
+    # @new_question = Question.new
+  # end
+
+  private
+
+  # Явно задаем список разрешенных параметров для модели User. Мы говорим, что
+  # у хэша params должен быть ключ :user. Значением этого ключа может быть хэш с
+  # ключами: :email, :password, :password_confirmation, :name, :username и
+  # :avatar_url. Другие ключи будут отброшены.
+  def user_params
+    params.require(:user).permit(:email,
+                                 :password,
+                                 :password_confirmation,
+                                 :name,
+                                 :username,
+                                 :avatar_url
     )
-
-    @questions = [
-      Question.new(text: 'Как дела?', created_at: Date.parse('13.09.2019')),
-      Question.new(text: 'В чём сила, брат?', created_at: Date.parse('14.09.2019')),
-      Question.new(text: 'Куришь?', created_at: Date.parse('15.09.2019')),
-      Question.new(text: 'Спишь?', created_at: Date.parse('15.09.2019')),
-      Question.new(text: 'Ешь?', created_at: Date.parse('15.09.2019'))
-    ]
-
-    @new_question = Question.new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, except: [:destroy] # Ресурс пользователей (экшен destroy не поддерживается)
   resources :questions
   root to: 'users#index'
-
-  # get 'users/index'
-  # get 'users/new'
-  # get 'users/edit'
-  # get 'users/show'
-  # get 'show' => 'users#show'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
