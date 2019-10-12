ef1058da222a4700f86c340c74755c2b89ee402b
occmium <occkart@yahoo.com>
2019-10-12 16:20:37 +0600

    Lesson-59 Shortcut routes SECUrity fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :users # Ресурс пользователей c поддержкой удаления
-  resources :sessions, only: [:new, :create, :destroy] # Ресурс сессий (только три экшена :new, :create, :destroy)
+  # resources :sessions, only: [:new, :create, :destroy] # Ресурс сессий (только три экшена :new, :create, :destroy)
+  resource :session, only: [:new, :create, :destroy]
+  # Ресурсы сессий во множественном числе, хотя у
+  # каждого юзера только одна собственная сессия. Чужие сессии он видеть не
+  # может, в отличие от юзеров: чужие профили он просматривать может. Для
+  # ресурсов у Рельсов есть хелпер и в единственном числе
   resources :questions, except: [:show, :new, :index] # Ресурс вопросов (кроме экшенов :show, :new, :index)
   resources :hashtags, only: [:show], param: :name
 
@@ -10,7 +15,8 @@ Rails.application.routes.draw do
   #
   # Для любознательных: синонимы мы добавили, чтобы показать одну вещь и потом
   # их удалим.
-  get 'sign_up' => 'users#new'
-  get 'log_out' => 'sessions#destroy'
-  get 'log_in' => 'sessions#new'
+  # Из раутов удаляем синонимы, которые не соответствуют REST
+  # get 'sign_up' => 'users#new'
+  # get 'log_out' => 'sessions#destroy'
+  # get 'log_in' => 'sessions#new'
 end
